92ea5e000ac1f859e468509ca59243ccd5b1d554
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2024-10-21 19:11:04 -0300

    Metodo Romberg y correcciones

--- Romberg.py
+++ Romberg.py
@@ -1,5 +1,34 @@
 import math
+import Trapecios as tp
 
-def metodo(a:float, b:float,n:int, x_values:list, y_values:list)->float:
-    pass
-def calcularT0(f,n)
+def romberg(f, a, b, e=1e-3):
+    """
+    a:Numero inferior del intervalo
+    b:Numero superior del intervalo
+    f:funcion ha integrar
+    e:tolerancia de error del metodo
+    """
+    R = [tp.trapecio_simple(a,b,f)]  # Primera aproximación trapezoidal
+    n = 1
+    error = float('inf')
+
+    while error > e:
+        h = (b - a) / 2 ** n
+        trapecio_suma = 0
+
+        # Suma los valores de la función en los puntos intermedios
+        for k in range(1, 2 ** n, 2):
+            trapecio_suma += f(a + k * h)
+        
+        # Nueva estimación usando la regla trapecio
+        i = 0.5 * R[-1] + h * trapecio_suma
+        error = abs(i - R[-1])
+        R.append(i)
+        n += 1
+
+    return R[-1]
+
+a = 0 
+b = math.pi 
+resultado = round(romberg(lambda x:math.sin(x), a, b),6)
+print(f"Resultado de la integral: {resultado}")
--- Trapecios.py
+++ Trapecios.py
@@ -11,16 +11,24 @@ def trapecio_simple(a:float, b:float, f) ->float:
     return h*(f(a) + f(b))
 
 def trapecio_complejo(a:float, b:float, f, n:int)->float:
+    """
+    a:Numero inferior del intervalo
+    b:Numero superior del intervalo
+    f:Funcion ha integrar
+    n:Numero de fracciones del intervalo
+    """
     h=(b-a)/n
     j:float=a
     suma=f(j)
     for i in range(0,n-1):
-        print(j)
         j = j + h
         suma = suma + 2*f(j)
     suma = suma + f(b)
     
     return h*(suma/2)
+def main():
+    print(f"El area de la funcion por trapecio simple es: {trapecio_simple(1.0,2.0,lambda x: math.exp(x))}")
+    print(f"El area de la funcion por trapecio complejo es: {trapecio_complejo(1.0,2.0, lambda x: math.exp(x),4)}")
 
-print(f"El area de la funcion por trapecio simple es: {trapecio_simple(1.0,2.0,lambda x: math.exp(x))}")
-print(f"El area de la funcion por trapecio complejo es: {trapecio_complejo(1.0,2.0, lambda x: math.exp(x),4)}")
+if __name__ == '__main__':
+    main()
